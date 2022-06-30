da0ae9d802d114c65cb52fe07062a4220928030e
Austin <austiny@snu.ac.kr>
2022-06-30 17:49:51 +0900

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,49 @@
+/*
+2022-06-30 
+첫 자바 프로그램
+By Austin
+*/
+
+class Main {
+	public static void main(String[] args) {
+
+		
+
+
+
+
+
+
+
+
+
+
+		
+
+		// System.out.println("Hello world!");  // "Hello World" 출력
+		// //System.out.println("코드의 실행을 중지시킬 경우도 사용");
+
+
+
+		// int x = 10, y =20;
+		// int tmp = 0;
+
+		// System.out.println("x: "+ x + " y: "+ y +" tmp :" + tmp);
+
+		// tmp = x; // tmp =10
+		// x = y;   // x = 20
+		// y = tmp;   // y = 10
+
+		// // 파이썬  x, y = y, x
+		
+		// System.out.println("x: "+ x + " y: "+ y +" tmp :" + tmp);
+
+
+
+
+		
+
+		
+  }
+}
+
